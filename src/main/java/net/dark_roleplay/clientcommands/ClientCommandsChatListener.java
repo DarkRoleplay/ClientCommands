630bd99fdf50d83422c4f9c93e84552cf37bbdc2
PORTB <alexander.pandaman@gmail.com>
2022-01-29 11:48:27 +1100

    Update to 1.18

--- src/main/java/net/dark_roleplay/clientcommands/ClientCommandsChatListener.java
+++ src/main/java/net/dark_roleplay/clientcommands/ClientCommandsChatListener.java
@@ -3,8 +3,9 @@ package net.dark_roleplay.clientcommands;
 import com.mojang.brigadier.ParseResults;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
-import net.minecraft.command.CommandSource;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,13 +17,14 @@ public class ClientCommandsChatListener {
 	public static void playerChat(ClientChatEvent event){
 		if(event.getMessage().startsWith(ClientCommands.getMarker() + "")){
 			try{
-				ClientPlayerEntity player = Minecraft.getInstance().player;
-				ClientCommandSource source = new ClientCommandSource(player, player.getPositionVec(), player.getPitchYaw(), null, 4, player.getName().getString(), player.getDisplayName(), null, player);
+				LocalPlayer player = Minecraft.getInstance().player;
+				ClientCommandSource source = new ClientCommandSource(player, player.position(), player.getRotationVector(), null, 4, player.getName().getString(), player.getDisplayName(), null, player);
 
-				ParseResults<CommandSource> parse = ClientCommands.getCommandDispatcher().parse(event.getMessage().substring(1), source);
+				ParseResults<CommandSourceStack> parse = ClientCommands.getCommandDispatcher().parse(event.getMessage().substring(1), source);
+
 				if(parse.getContext().getNodes().size() > 0){
 					event.setCanceled(true);
-					Minecraft.getInstance().ingameGUI.getChatGUI().addToSentMessages(event.getOriginalMessage());
+					Minecraft.getInstance().gui.getChat().addRecentChat(event.getOriginalMessage());
 					ClientCommands.getCommandDispatcher().execute(parse);
 				}
 			} catch (CommandSyntaxException e) {}
